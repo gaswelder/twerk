a188a018f5579bd2e2a80e63b40f60a4f914674e
gasweld <gasweld@gmail.com>
2019-07-01 01:37:09 +0300

    Cleaner config parser

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 )
@@ -12,6 +13,7 @@ func parseConfig(path string) (twerks, error) {
 		return nil, fmt.Errorf("failed to read %s: %v", path, err)
 	}
 
+	// Parse into an opaque string->?? map first.
 	kv := make(map[string]json.RawMessage)
 	err = json.Unmarshal(data, &kv)
 	if err != nil {
@@ -20,20 +22,29 @@ func parseConfig(path string) (twerks, error) {
 
 	cfg := make(twerks)
 	for name, data := range kv {
-		c := make(compositeTwerk, 0)
-		t := new(twerk)
-		err := json.Unmarshal(data, &c)
-		if err == nil {
-			cfg[name] = c
-			continue
+		t, err := parseConfigNode(data)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse twerks[%v]: %v", name, err)
 		}
-		err = json.Unmarshal(data, &t)
-		if err == nil {
-			cfg[name] = t
-			continue
-		}
-		return nil, fmt.Errorf("failed to parse twerks[%v]: %v", name, err)
+		cfg[name] = t
 	}
 
 	return cfg, nil
+}
+
+func parseConfigNode(data json.RawMessage) (twerkable, error) {
+	// Is it a composite?
+	c := make(compositeTwerk, 0)
+	err := json.Unmarshal(data, &c)
+	if err == nil {
+		return c, nil
+	}
+
+	// Is it a regular twerk?
+	t := new(twerk)
+	err = json.Unmarshal(data, &t)
+	if err == nil {
+		return t, nil
+	}
+	return nil, errors.New("unrecognized node format")
 }
